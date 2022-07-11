4a98fda52d22848c458c222341e5408db39d800b
Lucas <107816056+LucasSGonza@users.noreply.github.com>
2022-07-11 18:10:30 -0300

    Add files via upload

---
+++ src/Principal/Principal.java
@@ -0,0 +1,13 @@
+package Principal;
+
+import View.TelaPrincipal;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+        
+    }
+}
